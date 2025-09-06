7aca790ee6668ad8e51410fc0a09c6be5055efac
Gurjyot Singh Raina <142009952+GurjyotSingh21@users.noreply.github.com>
2025-09-06 15:11:23 +0530

    Update MusicLibrary.jsx

--- src/MusicLibrary.jsx
+++ src/MusicLibrary.jsx
@@ -82,7 +82,7 @@ const MusicLibrary = ({ role }) => {
   };
 
   return (
-    <div className="items-center min-height-screen justify-center bg-black text-white">
+    <div className="items-center min-height-screen max-w-[80vh] justify-center bg-black text-white">
     <div className="bg-neutral-900 min-w-3xl rounded-2xl shadow-2xl p-10 w-full max-w-md border border-green-500">
     <div className="max-w-3xl mx-auto p-4 bg-grey-800 shadow-md rounded-lg">
       <h2 className="text-3xl font-bold text-green-400 mb-4">Music Library</h2>
