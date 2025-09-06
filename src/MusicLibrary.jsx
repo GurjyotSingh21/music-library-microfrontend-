7bc339b6ebd4d82930194341489e6d894425609a
Gurjyot Singh Raina <142009952+GurjyotSingh21@users.noreply.github.com>
2025-09-06 15:36:38 +0530

    Update MusicLibrary.jsx

--- src/MusicLibrary.jsx
+++ src/MusicLibrary.jsx
@@ -81,7 +81,7 @@ const MusicLibrary = ({ role }) => {
     setSongs(songs.filter((song) => song.id !== id));
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white px-4">
+    <div className="flex items-center justify-center min-h-screen bg-neutral-900 text-white px-4">
       <div className="w-full max-w-4xl mx-auto shadow-md rounded-2xl border border-green-500 bg-neutral-900">
     <div className="max-w-3xl mx-auto p-4 bg-grey-800 shadow-md rounded-lg">
       <h2 className="text-3xl font-bold text-green-400 mb-4">Music Library</h2>
